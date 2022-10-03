b1509ee21488589db479cee1d00c72772b5e55e3
OmaidQ <qureshmu@tcd.ie>
2022-10-03 19:43:29 +0100

    Update input.java
    Implemented method which checks if string is a valid expression for calculation

--- input.java
+++ input.java
@@ -1,26 +1,45 @@
 import java.util.Scanner;
 
-
 public class input {
 
-	public static void main(String[] args) {
-		System.out.print("Enter the numeric expression you would like to evaluate:");
-		Scanner input = new Scanner( System.in );
-        String value = input.next();
-        boolean check = isNumeric(value);
-		input.close();
-		
-		System.out.println(check);
-		
+    public static void main(String[] args) {
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("Enter the numeric expression you would like to evaluate: ");
+            Scanner input = new Scanner(System.in);
+            String expression = input.next();
+            if (isValid(expression))
+                System.out.println("Success");
+            else {
+                System.out.println("Invalid expression to evaluate");
+                System.out.println("Type 'exit' if you would like to leave the calculator, if not press enter: ");
+                String s = input.next();
+                if (s.equalsIgnoreCase("exit")) {
+                    exit = true;
+                }
+            }
+        }
 
-    } 
-    
-    public static boolean isNumeric(String string) {
-        try {
-            Double.parseDouble(string);
-            return true;
-        }   catch(NumberFormatException e){
+    }
+
+    public static boolean isValid(String exp) {
+        char lastChar = '1';
+        int n = exp.length() - 1;
+        if (exp.charAt(0) == '+' | exp.charAt(0) == '-' | exp.charAt(0) == '*')
             return false;
-        }    
+        for (char c : exp.toCharArray()) {
+            if (c >= '0' && c <= '9') {
+                lastChar = c;
+            } else if (c == '+' | c == '-' | c == '*') {
+                if (lastChar == '+' | lastChar == '-' | lastChar == '*')
+                    return false;
+                else
+                    lastChar = c;
+            } else
+                return false;
+        }
+        if (exp.charAt(n) == '+' | exp.charAt(n) == '-' | exp.charAt(n) == '*')
+            return false;
+        return true;
     }
 }
