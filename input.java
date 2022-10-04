741f7fd80b36e83010cf5f1bc664745969c7190d
OmaidQ <qureshmu@tcd.ie>
2022-10-04 01:00:34 +0100

    Update input.java
    Implemented infix to postfix on string

--- input.java
+++ input.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class input {
 
@@ -8,9 +9,10 @@ public class input {
             System.out.println("Enter the numeric expression you would like to evaluate: ");
             Scanner input = new Scanner(System.in);
             String expression = input.next();
-            if (isValid(expression))
-                System.out.println("Success");
-            else {
+            if (isValid(expression)) {
+                String postfix = convertToPostfix(expression);
+                System.out.println(postfix);
+            } else {
                 System.out.println("Invalid expression to evaluate");
                 System.out.println("Type 'exit' if you would like to leave the calculator, if not press enter: ");
                 String s = input.next();
@@ -25,21 +27,52 @@ public class input {
     public static boolean isValid(String exp) {
         char lastChar = '1';
         int n = exp.length() - 1;
-        if (exp.charAt(0) == '+' | exp.charAt(0) == '-' | exp.charAt(0) == '*')
+        if (isOperator(exp.charAt(0)))
             return false;
         for (char c : exp.toCharArray()) {
-            if (c >= '0' && c <= '9') {
+            if (Character.isDigit(c)) {
                 lastChar = c;
-            } else if (c == '+' | c == '-' | c == '*') {
-                if (lastChar == '+' | lastChar == '-' | lastChar == '*')
+            } else if (isOperator(c)) {
+                if (isOperator(lastChar))
                     return false;
                 else
                     lastChar = c;
             } else
                 return false;
         }
-        if (exp.charAt(n) == '+' | exp.charAt(n) == '-' | exp.charAt(n) == '*')
+        if (isOperator(exp.charAt(n)))
             return false;
         return true;
+    }
+
+    public static String convertToPostfix(String infix) {
+        StringBuilder postfix = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < infix.length(); i++) {
+            char c = infix.charAt(i);
+            if (Character.isDigit(c)) {
+                postfix.append(c);
+            } else if (isOperator(c)) {
+                postfix.append(' ');
+                while (!stack.isEmpty() && c >= stack.peek()) {// + -* 42 45 41, - *+ 45 41 42, * +- 41 42 45
+                    postfix.append(stack.pop());
+                    postfix.append(' ');
+                }
+                stack.push(c);
+            }
+        }
+        postfix.append(' ');
+        while (!stack.isEmpty()) {
+            postfix.append(stack.pop());
+            postfix.append(' ');
+        }
+        return postfix.toString();
+    }
+
+    public static boolean isOperator(char c) {
+        if (c == '+' | c == '-' | c == '*')
+            return true;
+        else
+            return false;
     }
 }
